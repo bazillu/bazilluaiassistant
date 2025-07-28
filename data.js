52f87380bc0554a532dc911ee7189d5ca7923f92
bazillu <bazilluincorporation@gmail.com>
2025-07-28 09:01:32 +0100

    edit

--- data.js
+++ data.js
@@ -1,4 +1,11 @@
-var data = {
-    question: 'What is the capital of Australia?',
-    answer: 'The capital of Australia is Canberra.'
-}
+var data = [
+    {
+        question: 'What is the capital of Australia?',
+        answer: 'The capital of Australia is Canberra.'
+    },
+    {
+        question: 'What is the capital of France?',
+        answer: 'The capital of France is Paris.'
+    }
+    // Add more question-answer pairs here as needed
+];
